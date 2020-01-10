d274cab07a72214a866c85bc5c7a8e0f16603f39
Mayank Sharma <Mayank@SeanergyMacPro008s-MacBook-Pro.local>
2020-01-10 08:28:35 +0530

    Eslint with airbnb style guide added

---
+++ config/webpack/rules.js
@@ -0,0 +1,25 @@
+//
+
+module.exports=[
+    {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+    }
+
+]
