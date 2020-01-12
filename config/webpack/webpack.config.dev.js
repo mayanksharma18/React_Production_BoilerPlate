af67097f3041a8786ffe67fa25a671d69c461724
Mayank Sharma <Mayank@SeanergyMacPro008s-MacBook-Pro.local>
2020-01-13 00:12:58 +0530

    Added Jest,Enzyme,a test case and few sctipts in package.json

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -4,7 +4,7 @@ const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig,{
     mode:"development",
-    // devtool: 'source-map',
+    devtool: 'source-map',
     plugins: [
         new webpack.LoaderOptionsPlugin({
           minimize: true,
