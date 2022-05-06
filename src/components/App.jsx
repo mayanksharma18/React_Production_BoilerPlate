9ff1caa702e0d07d0cfbe9f61a120bbe0d2c557f
mayanksharma18 <mmayank.sharma18@gmail.com>
2022-05-06 11:42:15 +0530

    Added mywork url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { useIsAuthenticated } from "@azure/msal-react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { handleLogin } from "../utils/azureAD";
-import Dashboard from "./Dashboard/Dashboard";
+// import Dashboard from "./Dashboard/Dashboard";
 
 const App = () => {
   const { instance } = useMsal();
   const isAuthenticated = useIsAuthenticated();
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
   if (isAuthenticated) {
-    navigate("/dashboard");
+    window.location.assign("https://mywork.seanergydigital.com/Dashboard/EmployeeDashBoard");
     return null;
   }
   return <button onClick={() => handleLogin(instance, "popup")}>Login</button>;
